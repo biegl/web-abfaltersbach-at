e3e6d80694c916d023ffcb672feeb4e2339b20c8
Markus Bürgler <markus.buergler@mohemian.com>
2020-09-02 07:02:15 +0200

    Update API host

--- resources/easyadmin/src/config.ts
+++ resources/easyadmin/src/config.ts
@@ -1,3 +1,3 @@
 export default class Config {
-    static host = "http://localhost:8000";
+    static host = process.env.NODE_ENV === 'production' ? "//test.abfaltersbach.at" : "//localhost:8000";
 }
