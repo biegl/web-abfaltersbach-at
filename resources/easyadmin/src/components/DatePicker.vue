aab49cfff3c155b1713586a1d463be2428b4c738
Markus Bürgler <markus.buergler@mohemian.com>
2021-05-15 10:34:00 +0200

    Update datepicker component

--- resources/easyadmin/src/components/DatePicker.vue
+++ resources/easyadmin/src/components/DatePicker.vue
@@ -1,40 +1,50 @@
 <template>
-    <v-date-picker
-        :attributes="attrs"
-        v-bind:value="selectedDate"
-        v-on:input="selectDate($event)"
-        :min-date="this.minDate"
-        locale="de"
-        is-dark
-        :popover="{
-            placement: 'bottom',
-            visibility: 'click',
-        }"
-    >
-        <div class="input-group">
-            <input
-                type="text"
-                class="form-control form-control-sm"
-                v-bind:aria-describedby="helpId"
-                readonly
-                id="date"
-                v-bind:value="selectedDate | date"
-            />
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary btn-sm" type="button">
-                    <i class="fa fa-calendar"></i>
-                </button>
-                <button
-                    v-if="clearButton"
-                    class="btn btn-outline-secondary btn-sm"
-                    type="button"
-                    @click="clearDate"
-                >
-                    <i class="fa fa-ban"></i>
-                </button>
+    <div class="form-group" role="group">
+        <label v-bind:for="id" v-if="label" v-html="label"></label>
+        <v-date-picker
+            :attributes="attrs"
+            v-bind:value="selectedDate"
+            v-on:input="selectDate($event)"
+            :min-date="this.minDate"
+            locale="de"
+            is-dark
+            :popover="{
+                placement: 'bottom',
+                visibility: 'click',
+            }"
+        >
+            <div class="input-group">
+                <input
+                    type="text"
+                    class="form-control form-control-sm"
+                    readonly
+                    v-bind:id="id"
+                    v-bind:value="selectedDate | date"
+                />
+                <div class="input-group-append">
+                    <button
+                        class="btn btn-outline-secondary btn-sm"
+                        type="button"
+                    >
+                        <i class="fa fa-calendar"></i>
+                    </button>
+                    <button
+                        v-if="clearButton"
+                        class="btn btn-outline-secondary btn-sm"
+                        type="button"
+                        @click="clearDate"
+                    >
+                        <i class="fa fa-ban"></i>
+                    </button>
+                </div>
             </div>
-        </div>
-    </v-date-picker>
+        </v-date-picker>
+        <small
+            class="form-text text-muted w-100"
+            v-if="description"
+            v-html="description"
+        ></small>
+    </div>
 </template>
 
 <script lang="ts">
@@ -48,12 +58,17 @@ export default Vue.extend({
         event: "change",
     },
 
-    props: ["selectedDate", "helpId", "minDate", "clearButton"],
+    props: ["label", "selectedDate", "minDate", "clearButton", "description"],
 
     data() {
         return {
+            id: null,
             attrs: [],
         };
+    },
+
+    mounted() {
+        this.id = this._uid;
     },
 
     filters: {
