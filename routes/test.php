53d8378ccb1ce996ee9b50f56bdd3a512c3defc5
Markus Bürgler <markus.buergler@me.com>
2025-06-06 23:42:49 +0200

    feat(files): add feature tests for store file request

--- routes/test.php
+++ routes/test.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/unauthenticated', function () {
     return response()->json('Accept: application/json header missing', 400);
+});
+
+Route::post('/_test/store-file', function (\App\Http\Requests\StoreFile $request) {
+    return response()->json(['message' => 'ok']);
 }); 
