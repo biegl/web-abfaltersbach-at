46edf9854f6fc009d57d5a5b934abfea0f4dc4c7
Markus Bürgler <markus.buergler@me.com>
2025-06-06 23:13:49 +0200

    feat(home): add logic to fetch data for home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,25 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Navigation;
+use App\Models\News;
+use Carbon\Carbon;
+
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $navigation = Navigation::topLevel()->get();
+        $news = News::notExpired()->orderBy('date', 'desc')->limit(20)->get();
+        $grouped_events = Event::upcoming()->get()->groupBy(function ($d) {
+            return Carbon::parse($d->date)->formatLocalized('%B');
+        });
+
+        return view('home', compact('navigation', 'news', 'grouped_events'));
     }
 }
