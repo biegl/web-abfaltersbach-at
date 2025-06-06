f2f4679fa1710dbb4ead2c6c472f3e1b51e54f43
Markus Bürgler <markus.buergler@me.com>
2025-06-07 00:35:39 +0200

    feat(api): bring NewsController tests to 100%

--- tests/Feature/Api/NewsControllerTest.php
+++ tests/Feature/Api/NewsControllerTest.php
@@ -105,18 +105,12 @@ test('it deletes a news article with attachments', function () {
     $news = News::factory()->create();
     $file = UploadedFile::fake()->create('test.jpg');
 
-    // Mock the FilesController
-    $fileControllerMock = $this->mock(\App\Http\Controllers\Api\FilesController::class)->makePartial();
-    $fileModel = new \App\Models\File(['file' => 'test.jpg', 'title' => 'test.jpg']);
-    $fileControllerMock->shouldReceive('storeFile')->andReturn($fileModel);
-
     // Attach the file
     $request = new \Illuminate\Http\Request();
     $request->files->set('file', $file);
-    $fileRecord = $fileControllerMock->storeFile($request);
+    $fileRecord = (new \App\Http\Controllers\Api\FilesController())->storeFile($request);
     $news->attachments()->save($fileRecord);
 
-    Storage::disk('local')->put('test.jpg', 'test');
     Storage::disk('local')->assertExists($fileRecord->file);
 
     // Delete the news
