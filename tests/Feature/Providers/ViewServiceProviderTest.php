bce0d07e5c7b233cca11395005b7641cafb07b34
Markus Bürgler <markus.buergler@me.com>
2025-06-12 20:33:50 +0200

    test: add ViewServiceProvider tests for header image selection

---
+++ tests/Feature/Providers/ViewServiceProviderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Providers\ViewServiceProvider;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\View;
+
+test('it shares the correct header image path for winter', function () {
+    Carbon::setTestNow(Carbon::create(2023, 1, 15)); // A winter month
+
+    $provider = new ViewServiceProvider(app());
+    $provider->boot();
+
+    $this->assertEquals('/images/header/abfaltersbach_winter.jpeg', View::shared('headerImagePath'));
+
+    Carbon::setTestNow(); // Reset time
+});
+
+test('it shares the correct header image path for non-winter', function () {
+    Carbon::setTestNow(Carbon::create(2023, 6, 15)); // A non-winter month
+
+    $provider = new ViewServiceProvider(app());
+    $provider->boot();
+
+    $this->assertEquals('/images/header/abfaltersbach.jpeg', View::shared('headerImagePath'));
+
+    Carbon::setTestNow(); // Reset time
+}); 
