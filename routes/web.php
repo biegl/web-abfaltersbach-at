0cc112ebf169709ebcad80616eb8d0c5eaba3a12
Markus Bürgler <markus.buergler@me.com>
2025-06-12 20:33:40 +0200

    refactor: remove home controller and simplify routing

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\FilesController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Auth::routes(['confirm' => true, 'verify' => true]);
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/startseite', [PageController::class, 'landing']);
 Route::get('/files/{name}', [FilesController::class, 'download'])->name('download');
 Route::fallback([PageController::class, 'show']);
-
-if (app()->environment('testing')) {
-    Route::get('/login', function () {
-        return 'login page';
-    })->name('login');
-}
