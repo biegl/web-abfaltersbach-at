a469d9a170cacbe56bf96baa89302e9c7a09eced
Markus Bürgler <markus.buergler@me.com>
2026-07-06 15:49:48 +0200

    chore: upgrade laravel/framework to ^13.0, pest to ^4.0, and dependent packages

--- config/analytics.php
+++ config/analytics.php
@@ -3,7 +3,7 @@
 return [
 
     /*
-     * The view id of which you want to display data.
+     * The property id of which you want to display data.
      */
     'property_id' => env('ANALYTICS_VIEW_ID'),
 
